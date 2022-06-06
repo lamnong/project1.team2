69d006db25bfe0c2c79e44827864ac81710d0287
shayne.dcs <shayne.dcs@gmail.com>
2022-06-06 10:27:51 -0700

    Updated JS to reflect notes

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 
-// Camelcase
+// Creating Function for Search Button to tied API Keys
 function searchButton() {
-  var userInput = document.getElementById ("search_input").value
+  // Creating Variables for API Keys and User Input
+  var userInput = document.getElementById("search_input").value
   console.log(userInput)
   var apiGiphyKey = "RBndWFUU3JZNPgZpC5u2EuWLAEZRQ1eJ"
-  var apiSuperHeroKey ="10221123479452152"
+  var apiSuperHeroKey = "10221123479452152"
   var superHeroApi = `https://superheroapi.com/api.php/${apiSuperHeroKey}`
   var giphy_Api = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${apiGiphyKey}`
 
-  // async
+  // Taking User Input from Search Button and applying to Super Hero API to get results
   let searchUrl = `${superHeroApi}/search/${userInput}`
 
-  // EMPTY PREVIOUS RESULTS
-  SUPERHEROAPI_RESULTS = {}
 
-  // FETCH SUPERHERO -> STORE DATA -> RENDER
+  // This is telling the Super Hero API to send us the requested data from userInput
   fetch(searchUrl)
     .then((data) => data.json())
     .then((json) => {
-     
-      
+
+
       updateDetailsBox(json.results[0])
-    
+
     })
 
-  // FETCH GIPHY -> RENDER
-  fetch(giphy_Api).then(function(data){
+  // This is telling the Giphy API to send us the requested data based on userInput
+  fetch(giphy_Api).then(function (data) {
     console.log(data)
     return data.json()
   })
-  .then(function (responseBody) {
-    var giphyContainer = document.getElementsByClassName("giphy-results")[0]
+    .then(function (responseBody) {
+      var giphyContainer = document.getElementsByClassName("giphy-results")[0]
 
-    giphyContainer.innerHTML = ""
+      giphyContainer.innerHTML = ""
 
-
-    for (let imageInfo of responseBody.data.slice(0, 8)) {
-      var imgPath = imageInfo.images.fixed_height.url
-      var img = document.createElement("img")
-      img.setAttribute("class", "giphy-img")
-      img.setAttribute("src", imgPath)
-      giphyContainer.append(img)
-    }
-  })
+      // Tells API to only provide 10 results per userInput 
+      for (let imageInfo of responseBody.data.slice(0, 10)) {
+        var imgPath = imageInfo.images.fixed_height.url
+        var img = document.createElement("img")
+        img.setAttribute("class", "giphy-img")
+        img.setAttribute("src", imgPath)
+        giphyContainer.append(img)
+      }
+    })
 }
-  
+// Takes Data from API and applies it to the placeholders designated in HTML 
 function updateDetailsBox(current) {
+  // This applies data from Super Hero API to placeholders
   let detailsImage = document.createElement("img")
   detailsImage.className = "details-hero-image"
   detailsImage.src = current.image.url
@@ -55,7 +55,7 @@ function updateDetailsBox(current) {
   let detailsTitle = document.createElement("h3")
   detailsTitle.className = "details-title"
   detailsTitle.innerText = current.name
-  
+
   let description = document.createElement("p")
   description.className = "description"
   description.innerText = `
@@ -63,14 +63,14 @@ function updateDetailsBox(current) {
     Place of birth:${current.biography["place-of-birth"]}
     Publisher:${current.biography.publisher}
   `
-
+  // This applies data from Giphy API to placesholders 
   let indentDiv = document.createElement("div")
   indentDiv.className = "indent"
   indentDiv.append(detailsTitle, description)
 
   let detailsBox = document.getElementsByClassName("details-box")[0]
+  detailsBox.innerHTML = ''
   detailsBox.append(detailsImage, indentDiv)
   detailsBox.style.padding = "30px"
 
- 
 }
